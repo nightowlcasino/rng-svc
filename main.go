e49c6cc728c34fa04fd6237a2a5069fb61c54b0f
supremedacos <nightowlcasino@protonmail.com>
2022-05-29 13:42:19 -0400

    fix spacing

--- main.go
+++ main.go
@@ -19,17 +19,17 @@ import (
 )
 
 const (
-	SLICE_SIZE = 20
-	getBlockTxsEndpoint = "/blocks/"
-	oracleAddress       = "4FC5xSYb7zfRdUhm6oRmE11P2GJqSMY8UARPbHkmXEq6hTinXq4XNWdJs73BEV44MdmJ49Qo"
-	oracleTxs           = "https://api.ergoplatform.com/api/v1/addresses/4FC5xSYb7zfRdUhm6oRmE11P2GJqSMY8UARPbHkmXEq6hTinXq4XNWdJs73BEV44MdmJ49Qo/transactions"
+  SLICE_SIZE = 20
+  getBlockTxsEndpoint = "/blocks/"
+  oracleAddress       = "4FC5xSYb7zfRdUhm6oRmE11P2GJqSMY8UARPbHkmXEq6hTinXq4XNWdJs73BEV44MdmJ49Qo"
+  oracleTxs           = "https://api.ergoplatform.com/api/v1/addresses/4FC5xSYb7zfRdUhm6oRmE11P2GJqSMY8UARPbHkmXEq6hTinXq4XNWdJs73BEV44MdmJ49Qo/transactions"
   ergUtxoEndpoint     = "https://node.nightowlcasino.io/utxo/byId/"
-	rouletteErgoTree    = "1012040004000404054a0e203eff84aa4780a9cc612ed429b28c0cb2d17d5d6372a84c4050e68d234743042b0e20afd0d6cb61e86d15f2a0adc1e7e23df532ba3ff35f8ba88bed16729cae9330320400040205040404050f05120406050604080509050c040ad807d601b2a5730000d602b2db63087201730100d603e4c6a70404d6049e7cdb6801b2db6502fe9999a38cc7a7017302007303d6057ee4c6a7050405d6069972057204d607cb7304d1ed96830201938c7202017305938c7202028cb2db6308a7730600029597830501ed9372037307939e720473087205eded9372037309927206730a907206730bed937203730c939e7204730d7205eded937203730e927206730f9072067310ed9372037311937205720493c27201720793c272017207"
-	minerFee            = 1500000 // 0.0015 ERG
+  rouletteErgoTree    = "1012040004000404054a0e203eff84aa4780a9cc612ed429b28c0cb2d17d5d6372a84c4050e68d234743042b0e20afd0d6cb61e86d15f2a0adc1e7e23df532ba3ff35f8ba88bed16729cae9330320400040205040404050f05120406050604080509050c040ad807d601b2a5730000d602b2db63087201730100d603e4c6a70404d6049e7cdb6801b2db6502fe9999a38cc7a7017302007303d6057ee4c6a7050405d6069972057204d607cb7304d1ed96830201938c7202017305938c7202028cb2db6308a7730600029597830501ed9372037307939e720473087205eded9372037309927206730a907206730bed937203730c939e7204730d7205eded937203730e927206730f9072067310ed9372037311937205720493c27201720793c272017207"
+  minerFee            = 1500000 // 0.0015 ERG
 )
 
 type ErgBoxIds struct {
-	Items []ErgTx `json:"items"`
+  Items []ErgTx `json:"items"`
 }
 
 type ErgTx struct {
@@ -39,43 +39,43 @@ type ErgTx struct {
 
 type ErgTxOutput struct {
   BoxId               string    `json:"boxId"`
-	AdditionalRegisters Registers `json:"additionalRegisters"`
+  AdditionalRegisters Registers `json:"additionalRegisters"`
 }
 
 type ErgTxOutputNode struct {
-	BoxId               string        `json:"boxId"`
-	Assets              []Tokens      `json:"assets"`
-	AdditionalRegisters RegistersNode `json:"additionalRegisters"`
-	ErgoTree            string        `json:"ergoTree"`
+  BoxId               string        `json:"boxId"`
+  Assets              []Tokens      `json:"assets"`
+  AdditionalRegisters RegistersNode `json:"additionalRegisters"`
+  ErgoTree            string        `json:"ergoTree"`
 }
 
 type Tokens struct {
-	TokenId string `json:"tokenId"`
-	Amount  int    `json:"amount"`
+  TokenId string `json:"tokenId"`
+  Amount  int    `json:"amount"`
 }
 
 type serialized struct {
-	BoxId string `json:"boxId"`
-	Bytes string `json:"bytes"`
+  BoxId string `json:"boxId"`
+  Bytes string `json:"bytes"`
 }
 
 type Registers struct {
-	R4 RegR4 `json:"R4"`
-	R5 RegR5 `json:"R5"`
+  R4 RegR4 `json:"R4"`
+  R5 RegR5 `json:"R5"`
 }
 
 type RegistersNode struct {
-	R4 string `json:"R4"`
-	R5 string `json:"R5"`
-	R6 string `json:"R6"`
+  R4 string `json:"R4"`
+  R5 string `json:"R5"`
+  R6 string `json:"R6"`
 }
 
 type RegR4 struct {
-	Value string `json:"renderedValue"`
+  Value string `json:"renderedValue"`
 }
 
 type RegR5 struct {
-	Value string `json:"renderedValue"`
+  Value string `json:"renderedValue"`
 }
 
 type CombinedHashes struct {
@@ -90,10 +90,10 @@ type ergBlockRandNum struct {
 var allErgBlockRandNums ergBlockRandNum
 
 func (e *ergBlockRandNum) get(key string) (string, bool) {
-	e.mu.Lock()
+  e.mu.Lock()
   defer e.mu.Unlock()
-	val, ok := e.randNums[key]
-	return val, ok
+  val, ok := e.randNums[key]
+  return val, ok
 }
 
 func (e *ergBlockRandNum) delete(key string) {
@@ -197,34 +197,34 @@ func postErgOracleTx(client *retryablehttp.Client, payload []byte) ([]byte, erro
 }
 
 func sendRandNum(w http.ResponseWriter, req *http.Request) {
-	start := time.Now()
+  start := time.Now()
   w.Header().Set("Access-Control-Allow-Origin", "*")
   w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	boxId := req.URL.Query().Get("boxId")
-	walletAddr := req.URL.Query().Get("walletAddr")
-	game := mux.Vars(req)["game"]
+  boxId := req.URL.Query().Get("boxId")
+  walletAddr := req.URL.Query().Get("walletAddr")
+  game := mux.Vars(req)["game"]
   reqURL := req.URL
   urlPath := reqURL.Path
   logger.WithFields(log.Fields{"caller": "sendRandNum", "urlPath": urlPath, "boxId": boxId, "game": game, "walletAddr": walletAddr}).Info("sendRandNum called")
 
-	go func(game string, boxId string, walletAddr string) {
-		timeout := time.NewTicker(60 * time.Second)
-		for {
-			select {
-			case <-timeout.C:
+  go func(game string, boxId string, walletAddr string) {
+    timeout := time.NewTicker(60 * time.Second)
+    for {
+      select {
+      case <-timeout.C:
         logger.WithFields(log.Fields{"caller": "sendRandNum", "durationMs": time.Since(start).Milliseconds(), "boxId": boxId, "game": game, "walletAddr": walletAddr}).Error("timeout - random number not found")
-				return
-			default:
-				if randNum, ok := allErgBlockRandNums.get(boxId); ok {
-					topic := fmt.Sprintf("%s.%s", game, walletAddr)
-					nc.Publish(topic, []byte(randNum))
+        return
+      default:
+        if randNum, ok := allErgBlockRandNums.get(boxId); ok {
+          topic := fmt.Sprintf("%s.%s", game, walletAddr)
+          nc.Publish(topic, []byte(randNum))
           logger.WithFields(log.Fields{"caller": "sendRandNum", "durationMs": time.Since(start).Milliseconds(), "randNum": randNum, "boxId": boxId, "game": game, "walletAddr": walletAddr}).Info("successfully sent random number")
-					return
-				}
-				time.Sleep(5 * time.Second)
-			}
-		}
-	}(game, boxId, walletAddr)
+          return
+        }
+        time.Sleep(5 * time.Second)
+      }
+    }
+  }(game, boxId, walletAddr)
   
   w.WriteHeader(http.StatusOK)
   fmt.Fprintf(w, "")
@@ -232,12 +232,12 @@ func sendRandNum(w http.ResponseWriter, req *http.Request) {
 }
 
 func serializeErgBox(client *retryablehttp.Client, boxId string) (string, error) {
-	var bytes serialized
+  var bytes serialized
 
-	req, err := retryablehttp.NewRequest("GET", "https://node.nightowlcasino.io/utxo/withPool/byIdBinary/" + boxId, nil)
-	if err != nil {
-		return bytes.Bytes, fmt.Errorf("error creating getErgBoxes request - %s", err.Error())
-	}
+  req, err := retryablehttp.NewRequest("GET", "https://node.nightowlcasino.io/utxo/withPool/byIdBinary/" + boxId, nil)
+  if err != nil {
+    return bytes.Bytes, fmt.Errorf("error creating getErgBoxes request - %s", err.Error())
+  }
   req.SetBasicAuth(nodeUser, nodePassword)
   req.Header.Set("no-scanner-func", "serializeErgBox")
 
@@ -260,12 +260,12 @@ func serializeErgBox(client *retryablehttp.Client, boxId string) (string, error)
 }
 
 func getErgUtxoBox(client *retryablehttp.Client, boxId string) (ErgTxOutputNode, error) {
-	var utxo ErgTxOutputNode
+  var utxo ErgTxOutputNode
 
-	req, err := retryablehttp.NewRequest("GET", "https://node.nightowlcasino.io/utxo/byId/" + boxId, nil)
-	if err != nil {
-		return utxo, fmt.Errorf("error creating getErgBoxes request - %s", err.Error())
-	}
+  req, err := retryablehttp.NewRequest("GET", "https://node.nightowlcasino.io/utxo/byId/" + boxId, nil)
+  if err != nil {
+    return utxo, fmt.Errorf("error creating getErgBoxes request - %s", err.Error())
+  }
   req.SetBasicAuth(nodeUser, nodePassword)
   req.Header.Set("no-scanner-func", "getErgUtxoBox")
 
@@ -288,34 +288,34 @@ func getErgUtxoBox(client *retryablehttp.Client, boxId string) (ErgTxOutputNode,
 }
 
 func buildResultSmartContractTx(betUtxo ErgTxOutputNode, r4, r5 uint64, betDataInput, oracleDataInput string) ([]byte, error) {
-	// Build Erg Tx for node to sign
-	assets := fmt.Sprintf(`{"tokenId": "%s", "amount": %d}`, betUtxo.Assets[0].TokenId, betUtxo.Assets[0].Amount)
-	txToSign := []byte(fmt.Sprintf(`{
-		"requests": [
-			{
-				"address": "%s",
-				"value": %d,
-				"assets": %s,
-				"registers": {
-					"R4": "04%02x",
-					"R5": "04%02x"
-				}
-			}
-		],
-		"fee": %d,
-		"inputsRaw": [
-			"%s"
-		],
-		"dataInputsRaw": [
-			"%s"
-		]
-	}`, betUtxo.AdditionalRegisters.R6, minerFee, assets, r4, r5, minerFee, betDataInput, oracleDataInput))
+  // Build Erg Tx for node to sign
+  assets := fmt.Sprintf(`{"tokenId": "%s", "amount": %d}`, betUtxo.Assets[0].TokenId, betUtxo.Assets[0].Amount)
+  txToSign := []byte(fmt.Sprintf(`{
+    "requests": [
+      {
+        "address": "%s",
+        "value": %d,
+        "assets": %s,
+        "registers": {
+          "R4": "04%02x",
+          "R5": "04%02x"
+        }
+      }
+    ],
+    "fee": %d,
+    "inputsRaw": [
+      "%s"
+    ],
+    "dataInputsRaw": [
+      "%s"
+    ]
+  }`, betUtxo.AdditionalRegisters.R6, minerFee, assets, r4, r5, minerFee, betDataInput, oracleDataInput))
 
-	return txToSign, nil
+  return txToSign, nil
 }
 
 func encodeZigZag64(n uint64) uint64 {
-	return (n << 1) ^ (n >> 63)
+  return (n << 1) ^ (n >> 63)
 }
 
 func main() {
@@ -333,13 +333,13 @@ func main() {
     }
   }
 
-	if value, ok := os.LookupEnv("PORT"); ok {
+  if value, ok := os.LookupEnv("PORT"); ok {
     port = value
   } else {
     port = "8089"
   }
 
-	if value, ok := os.LookupEnv("NATS_ENDPOINT"); ok {
+  if value, ok := os.LookupEnv("NATS_ENDPOINT"); ok {
     natsEndpoint = value
   } else {
     natsEndpoint = nats.DefaultURL
@@ -351,33 +351,33 @@ func main() {
   })
 
   // Connect to the nats server
-	var err error
+  var err error
   nc, err = nats.Connect(natsEndpoint)
   if err != nil {
     logger.WithFields(log.Fields{"error": err.Error()}).Fatal("failed to connect to ':4222' nats server")
   }
 
-	allErgBlockRandNums.randNums = make(map[string]string)
-	index = 0
+  allErgBlockRandNums.randNums = make(map[string]string)
+  index = 0
   
   nc.Subscribe("eth.hash", func(m *nats.Msg) {
-		var hash CombinedHashes
+    var hash CombinedHashes
     err = json.Unmarshal(m.Data, &hash)
     if err != nil {
       logger.WithFields(log.Fields{"error": err.Error()}).Error("failed to unmarshal CombinedHashes")
     } else {
-			combinedHashes[index % SLICE_SIZE] = hash
-			// the ERG BoxId random numbers are stored in a hash map and will be set to the 2nd ETH hash block Id
-			// from the initially associated one
-			if index >= 2 {
-				h := combinedHashes[(index-2) % SLICE_SIZE]
-				for _, boxId := range h.Boxes {
+      combinedHashes[index % SLICE_SIZE] = hash
+      // the ERG BoxId random numbers are stored in a hash map and will be set to the 2nd ETH hash block Id
+      // from the initially associated one
+      if index >= 2 {
+        h := combinedHashes[(index-2) % SLICE_SIZE]
+        for _, boxId := range h.Boxes {
           allErgBlockRandNums.randNums[boxId] = hash.Hash[2:10]
-				}
-			}
+        }
+      }
 
       // when the slice size has reached 20 we will begin to remove old ERG BoxIds from the hash map
-			if index >= 19 {
+      if index >= 19 {
         old := combinedHashes[(index+1) % SLICE_SIZE]
         for _, boxId := range old.Boxes {
           allErgBlockRandNums.delete(boxId)
@@ -385,14 +385,14 @@ func main() {
       }
 
       index++
-		}
+    }
   })
   logger.Info("subscribed to eth.hash")
 
   var ergTxs ErgBoxIds
-	var ergUtxo ErgTxOutputNode
+  var ergUtxo ErgTxOutputNode
 
-	t := &http.Transport{
+  t := &http.Transport{
     Dial: (&net.Dialer{
       Timeout: 3 * time.Second,
     }).Dial,
@@ -402,7 +402,7 @@ func main() {
     TLSHandshakeTimeout: 3 * time.Second,
   }
 
-	retryClient := retryablehttp.NewClient()
+  retryClient := retryablehttp.NewClient()
   retryClient.HTTPClient.Transport = t
   retryClient.HTTPClient.Timeout = time.Second * 3
   retryClient.Logger = nil
@@ -417,14 +417,14 @@ func main() {
   }
 
   // https://api.ergoplatform.com/api/v1/addresses/xxxx/transactions
-	txsReq, err := retryablehttp.NewRequest("GET", oracleTxs, nil)
+  txsReq, err := retryablehttp.NewRequest("GET", oracleTxs, nil)
   if err != nil {
     fmt.Println("failed to get oracle transactions")
   }
 
   resp, err := retryClient.Do(txsReq)
   if err != nil {
-		fmt.Printf("error calling ergo api explorer - %s\n", err.Error())
+    fmt.Printf("error calling ergo api explorer - %s\n", err.Error())
   }
   defer resp.Body.Close()
 
@@ -438,69 +438,69 @@ func main() {
     fmt.Printf("error unmarshalling erg txs - %s\n", err.Error())
   }
 
-	for _, ergTx := range ergTxs.Items {
+  for _, ergTx := range ergTxs.Items {
 
-		// convert R4 rendered value to []string
-		r4 := ergTx.Outputs[0].AdditionalRegisters.R4.Value
-		// remove surrounding brackets [ and ]
-	  r4 = strings.TrimPrefix(r4, "[")
-	  r4 = strings.TrimSuffix(r4, "]")
-		//ethHashIds := strings.Split(r4, ",")
+    // convert R4 rendered value to []string
+    r4 := ergTx.Outputs[0].AdditionalRegisters.R4.Value
+    // remove surrounding brackets [ and ]
+    r4 = strings.TrimPrefix(r4, "[")
+    r4 = strings.TrimSuffix(r4, "]")
+    //ethHashIds := strings.Split(r4, ",")
 
-		// convert R5 rendered value to [][]string
-		r5 := ergTx.Outputs[0].AdditionalRegisters.R5.Value
-	  // remove surrounding brackets [ and ]
-	  r5 = strings.TrimPrefix(r5, "[")
-	  r5 = strings.TrimSuffix(r5, "]")
-	  // add , to the back of string to help for the split
-		r5 = r5 + ","
-		ergBoxIdsSlices := strings.Split(r5, "],")
-		// remove last element because it's empty
-		ergBoxIdsSlices = ergBoxIdsSlices[:len(ergBoxIdsSlices)-1]
+    // convert R5 rendered value to [][]string
+    r5 := ergTx.Outputs[0].AdditionalRegisters.R5.Value
+    // remove surrounding brackets [ and ]
+    r5 = strings.TrimPrefix(r5, "[")
+    r5 = strings.TrimSuffix(r5, "]")
+    // add , to the back of string to help for the split
+    r5 = r5 + ","
+    ergBoxIdsSlices := strings.Split(r5, "],")
+    // remove last element because it's empty
+    ergBoxIdsSlices = ergBoxIdsSlices[:len(ergBoxIdsSlices)-1]
 
-	  for i, ergBoxIdsSlice := range ergBoxIdsSlices {
-			// remove leading [
-			ergBoxIdsClean := strings.TrimPrefix(ergBoxIdsSlice, "[")
-	  	ergBoxIds := strings.Split(ergBoxIdsClean, ",")
+    for i, ergBoxIdsSlice := range ergBoxIdsSlices {
+      // remove leading [
+      ergBoxIdsClean := strings.TrimPrefix(ergBoxIdsSlice, "[")
+      ergBoxIds := strings.Split(ergBoxIdsClean, ",")
 
-			if len(ergBoxIds) > 0 && ergBoxIds[0] != "" {
-				for j, boxId := range ergBoxIds {
-					start := time.Now()
-					utxoReq, err := retryablehttp.NewRequest("GET", ergUtxoEndpoint + boxId, nil)
-					if err != nil {
-						fmt.Println("failed to get roulette bet utxos")
-					}
+      if len(ergBoxIds) > 0 && ergBoxIds[0] != "" {
+        for j, boxId := range ergBoxIds {
+          start := time.Now()
+          utxoReq, err := retryablehttp.NewRequest("GET", ergUtxoEndpoint + boxId, nil)
+          if err != nil {
+            fmt.Println("failed to get roulette bet utxos")
+          }
           utxoReq.SetBasicAuth(nodeUser, nodePassword)
 
-					resp, err = retryClient.Do(utxoReq)
-					if err != nil {
-						fmt.Printf("error calling ergo node - %s\n", err.Error())
-					}
-					defer resp.Body.Close()
+          resp, err = retryClient.Do(utxoReq)
+          if err != nil {
+            fmt.Printf("error calling ergo node - %s\n", err.Error())
+          }
+          defer resp.Body.Close()
 
-					body, err = ioutil.ReadAll(resp.Body)
-					if err != nil {
-						fmt.Printf("error reading erg utxo body - %s\n", err.Error())
-					}
+          body, err = ioutil.ReadAll(resp.Body)
+          if err != nil {
+            fmt.Printf("error reading erg utxo body - %s\n", err.Error())
+          }
 
-					err = json.Unmarshal(body, &ergUtxo)
-					if err != nil {
-						fmt.Printf("error unmarshalling erg utxo - %s\n", err.Error())
-					}
+          err = json.Unmarshal(body, &ergUtxo)
+          if err != nil {
+            fmt.Printf("error unmarshalling erg utxo - %s\n", err.Error())
+          }
 
-					//fmt.Printf("r4: %s\nr5: %s\nr6: %s\n", ergUtxo.AdditionalRegisters.R4, ergUtxo.AdditionalRegisters.R5, ergUtxo.AdditionalRegisters.R6)
-					serializedOracleBox, _ := serializeErgBox(retryClient, ergTx.Outputs[0].BoxId)
-					serializedBetBox, _ := serializeErgBox(retryClient, ergUtxo.BoxId)
-					txUnsigned, _ := buildResultSmartContractTx(ergUtxo, encodeZigZag64(uint64(i)), encodeZigZag64(uint64(j)), serializedBetBox, serializedOracleBox)
-					txSigned, err := postErgOracleTx(retryClient, txUnsigned)
-					logger.WithFields(log.Fields{"caller": "postErgOracleTx", "durationMs": time.Since(start).Milliseconds(), "txId": string(txSigned)}).Info("successfully sent tx to result smart contract")
-				}
-			}
-	  }
-	}
+          //fmt.Printf("r4: %s\nr5: %s\nr6: %s\n", ergUtxo.AdditionalRegisters.R4, ergUtxo.AdditionalRegisters.R5, ergUtxo.AdditionalRegisters.R6)
+          serializedOracleBox, _ := serializeErgBox(retryClient, ergTx.Outputs[0].BoxId)
+          serializedBetBox, _ := serializeErgBox(retryClient, ergUtxo.BoxId)
+          txUnsigned, _ := buildResultSmartContractTx(ergUtxo, encodeZigZag64(uint64(i)), encodeZigZag64(uint64(j)), serializedBetBox, serializedOracleBox)
+          txSigned, err := postErgOracleTx(retryClient, txUnsigned)
+          logger.WithFields(log.Fields{"caller": "postErgOracleTx", "durationMs": time.Since(start).Milliseconds(), "txId": string(txSigned)}).Info("successfully sent tx to result smart contract")
+        }
+      }
+    }
+  }
 
   r := mux.NewRouter()
-	r.HandleFunc("/random-number/{game}", sendRandNum).Methods("GET")
+  r.HandleFunc("/random-number/{game}", sendRandNum).Methods("GET")
   http.Handle("/", r)
   logger.Infof("serving on port %s", port)
   http.ListenAndServe(":"+port, r)
